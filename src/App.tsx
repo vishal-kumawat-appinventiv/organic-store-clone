d82405a9b8ba02363bffe583b788a02f65d405fc
vishal-kumawat-appinventiv <vishal.kumawat@appinventiv.com>
2024-09-04 18:06:29 +0530

    react router dom installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import Navbar from "./components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./screens/HomePage";
 
 const App = () => {
-  return <Navbar />;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;

85a394b2ff5265426b646cf6a3a4a9f3a858bb13
vishal-kumawat-appinventiv <vishal.kumawat@appinventiv.com>
2024-09-05 16:41:41 +0530

    ui

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -85,16 +85,16 @@ const Footer = () => {
   return (
     <>
       <div className="bg-black text-[#aeaeae]">
-        <div className="mainGrid max-w-7xl mx-auto grid grid-cols-2 gap-4 py-20">
+        <div className="mainGrid max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-4 py-20 px-3 text-center md:text-left">
           <div className="logoCol">
             <img src={logoImg} alt="img" />
-            <p className="mt-10 italic mr-24 font-bold">
+            <p className="mt-10 italic mr-0 md:mr-24 font-bold">
               Maecenas mi justo, interdum at consectetur vel, tristique et arcu.
               Ut quis eros blandit, ultrices diam in, elementum ex. Suspendisse
               quis faucibus urna. Suspendisse pellentesque.
             </p>
           </div>
-          <div className="rightCol grid grid-cols-2 gap-3">
+          <div className="rightCol grid grid-cols-1 md:grid-cols-2 gap-3">
             <div>
               <div>
                 <h1 className="text-2xl text-white mb-10">Quick Links</h1>
@@ -144,7 +144,7 @@ const Footer = () => {
                   })}
                 </div>
               </div>
-              <div className="btns flex gap-3 w-full mt-4">
+              <div className="btns flex flex-col items-center lg:flex-row gap-3 w-full mt-4">
                 {btnImgs.map((b, idx) => {
                   return <img key={idx} src={b.img} alt="img" width="130" />;
                 })}
@@ -153,7 +153,7 @@ const Footer = () => {
           </div>
         </div>
         <div className="border-t-2 border-gray-500">
-          <div className="max-w-7xl mx-auto flex items-center justify-between py-5">
+          <div className="max-w-7xl mx-auto flex flex-col lg:flex-row items-center justify-between py-5">
             <p>Copyright © 2024 | Organic Store</p>
             <div className="flex gap-2">
               <Facebook />

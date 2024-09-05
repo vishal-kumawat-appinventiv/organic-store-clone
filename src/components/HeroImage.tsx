c76479de8ccd55222c8d07b5ef4a7f9ca5c5355a
vishal-kumawat-appinventiv <vishal.kumawat@appinventiv.com>
2024-09-05 10:42:23 +0530

    ui

--- src/components/HeroImage.tsx
+++ src/components/HeroImage.tsx
@@ -10,42 +10,45 @@ const HeroImage = () => {
   return (
     <>
       <div className="max-w-7xl w-full mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-2 py-32">
+        <div className="grid grid-cols-1 md:grid-cols-2 py-32 px-3">
           <div>
             <img src={heroImage} alt="heroImage" />
           </div>
-          <div className="flex flex-col w-full gap-5">
-            <div className="leafImg relative p-5">
-              <img
-                src={leafImage}
-                alt="leafHero"
-                width={70}
-                className="absolute top-0 left-0"
-              />
-            </div>
-            <div>
-              <h4 className="text-black font-bold text-xl">
-                Best Quality Products
-              </h4>
-            </div>
-            <div>
-              <h1 className="text-6xl  text-black">
-                Join The Organic Movement!
-              </h1>
-            </div>
-            <div>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
-                tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
-              </p>
-            </div>
-            <div className="ShopNowBtn py-8">
-              <button className="bg-[#6a9739] p-3 px-6 text-white rounded">
-                <span className="flex gap-2">
-                  <ShoppingCart />
-                  SHOP NOW
-                </span>
-              </button>
+          <div className="relative">
+            <div className="lg:absolute lg:right-0 lg:top-8 text-center md:text-left max-w-xl flex flex-col w-full gap-5">
+              <div className="leafImg relative p-5">
+                <img
+                  src={leafImage}
+                  alt="leafHero"
+                  width={70}
+                  className="absolute top-0 left-0"
+                />
+              </div>
+              <div>
+                <h4 className="text-black font-bold text-xl">
+                  Best Quality Products
+                </h4>
+              </div>
+              <div>
+                <h1 className="text-3xl lg:text-6xl font-bold  text-black">
+                  Join The Organic Movement!
+                </h1>
+              </div>
+              <div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                  elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus
+                  leo.
+                </p>
+              </div>
+              <div className="ShopNowBtn py-8">
+                <button className="bg-[#6a9739] p-3 px-6 text-white rounded">
+                  <span className="flex gap-2">
+                    <ShoppingCart />
+                    SHOP NOW
+                  </span>
+                </button>
+              </div>
             </div>
           </div>
         </div>

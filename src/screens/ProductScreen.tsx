24881ab843f278e814fb41c71bb12dc800b1702e
vishal-kumawat-appinventiv <vishal.kumawat@appinventiv.com>
2024-09-06 12:06:35 +0530

    ui

--- src/screens/ProductScreen.tsx
+++ src/screens/ProductScreen.tsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import { useContext, useMemo, useState } from "react";
 import { MyContext } from "../libs/MyContext";
@@ -66,6 +66,56 @@ const ProductScreen = () => {
                   <p>Category: {product?.category}</p>
                 </div>
               </div>
+            </div>
+          </div>
+          <div className="border-t mt-5 flex flex-col">
+            <h4 className="text-xl mt-3">Description</h4>
+            <p className="text-gray-600">{product?.desc}</p>
+          </div>
+          {/* related prod */}
+          <div>
+            <h1 className="my-5 text-3xl font-bold">Related products</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-3">
+              {products
+                .filter((p) => p.category === product?.category)
+                .slice(0, 3)
+                .map((p) => {
+                  return (
+                    <div
+                      key={p.id}
+                      className="relative flex flex-col text-center"
+                    >
+                      <Link
+                        to={`/product/${p.name
+                          .replace(/\s+/g, "-")
+                          .toLowerCase()}`}
+                      >
+                        <div className="w-full h-full">
+                          <img src={p.img} alt="img" className="w-full" />
+                        </div>
+                      </Link>
+                      <p className="text-gray-600">{p.category}</p>
+                      <Link
+                        to={`/product/${p.name
+                          .replace(/\s+/g, "-")
+                          .toLowerCase()}`}
+                      >
+                        <p className="font-bold">{p.name}</p>
+                      </Link>
+                      <div>
+                        {Array.from({ length: 5 }, (_, i) => (
+                          <span key={i}>☆</span>
+                        ))}
+                      </div>
+                      <p className="text-sm font-bold">£ {p.price}</p>
+                      {p.sale && (
+                        <div className="absolute bg-[#8cc44b] text-sm rounded-full py-2 px-1 top-[-3px] right-0">
+                          <p>Sale!</p>
+                        </div>
+                      )}
+                    </div>
+                  );
+                })}
             </div>
           </div>
         </div>

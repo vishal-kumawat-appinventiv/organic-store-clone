73c322445b9b7299e0079f253c2aae276bf82a8c
vishal-kumawat-appinventiv <vishal.kumawat@appinventiv.com>
2024-09-06 11:30:30 +0530

    ui

--- src/screens/ProductScreen.tsx
+++ src/screens/ProductScreen.tsx
@@ -1,8 +1,24 @@
+import { useLocation } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import { useContext, useMemo } from "react";
+import { MyContext } from "../libs/MyContext";
 
 const ProductScreen = () => {
-  return (
-    <div>ProductScreen</div>
-  )
-}
+  const location = useLocation();
+  const currentPath = location.pathname.split("/")[2].replace(/-/g, " ");
+  const { products } = useContext(MyContext);
 
-export default ProductScreen
+  const product = useMemo(() => {
+    return products.find((product) =>
+      product.name.toLowerCase().includes(currentPath)
+    );
+  }, [products, currentPath]);
+
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export default ProductScreen;

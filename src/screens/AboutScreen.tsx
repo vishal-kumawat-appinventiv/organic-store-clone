1e4268899996078d649dd91585b699072b582aea
vishal-kumawat-appinventiv <vishal.kumawat@appinventiv.com>
2024-09-06 16:31:56 +0530

    ui

--- src/screens/AboutScreen.tsx
+++ src/screens/AboutScreen.tsx
@@ -1,6 +1,9 @@
 import Navbar from "../components/Navbar";
 
 const AboutScreen = () => {
+  const leafImage =
+    "https://websitedemos.net/organic-shop-02/wp-content/uploads/sites/465/2021/03/basil-leaf.png";
+
   return (
     <>
       <Navbar />
@@ -9,34 +12,39 @@ const AboutScreen = () => {
           About Us
         </h1>
       </div>
-      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 p-3 gap-10 my-16">
-        <div className="flex flex-col gap-5 my-auto">
-          <div>
-            <h1 className="text-2xl md:text-4xl font-bold">
-              We Are Your Favourite Store.
-            </h1>
-          </div>
-          <div className="mr-5">
-            <p>
-              Tuas quisquam quo gravida proident harum, aptent ligula anim
-              consequuntur, ultrices mauris, nunc voluptates lobortis, varius,
-              potenti placeat! Fuga omnis. Cubilia congue. Recusandae. Vero
-              penatibus quasi! Nostra tenetur dignissimos ultrices natus
-              distinctio ultrices consequuntur numqu.
-              <br />
-              <br />
-              Officiis fuga harum porro et? Similique rhoncus atque! Netus
-              blanditiis provident nunc posuere. Rem sequi, commodo, lorem
-              tellus elit, hic sem tenetur anim amet quas, malesuada proident
-              platea corrupti expedita.
-            </p>
-          </div>
+      <div className="relative">
+        <div className="absolute top-[-30px] left-1/2 transform -translate-x-1/2">
+          <img width="150" height="90" src={leafImage} alt="leaf" />
         </div>
-        <div>
-          <img
-            src="https://websitedemos.net/organic-shop-02/wp-content/uploads/sites/465/2020/01/banner-01.jpg"
-            alt="img"
-          />
+        <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 p-3 gap-10 py-24">
+          <div className="flex flex-col gap-5 my-auto">
+            <div>
+              <h1 className="text-2xl md:text-4xl font-bold">
+                We Are Your Favourite Store.
+              </h1>
+            </div>
+            <div className="mr-5">
+              <p>
+                Tuas quisquam quo gravida proident harum, aptent ligula anim
+                consequuntur, ultrices mauris, nunc voluptates lobortis, varius,
+                potenti placeat! Fuga omnis. Cubilia congue. Recusandae. Vero
+                penatibus quasi! Nostra tenetur dignissimos ultrices natus
+                distinctio ultrices consequuntur numqu.
+                <br />
+                <br />
+                Officiis fuga harum porro et? Similique rhoncus atque! Netus
+                blanditiis provident nunc posuere. Rem sequi, commodo, lorem
+                tellus elit, hic sem tenetur anim amet quas, malesuada proident
+                platea corrupti expedita.
+              </p>
+            </div>
+          </div>
+          <div>
+            <img
+              src="https://websitedemos.net/organic-shop-02/wp-content/uploads/sites/465/2020/01/banner-01.jpg"
+              alt="img"
+            />
+          </div>
         </div>
       </div>
     </>

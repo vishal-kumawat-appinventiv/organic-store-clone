5d360ea62d388c6e1d2644c2a9a0e708261b9251
vishal-kumawat-appinventiv <vishal.kumawat@appinventiv.com>
2024-09-09 17:59:11 +0530

    redux-persist added

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "../store.ts";
+import { store, persistor } from "../store.ts";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")!).render(
   <>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </>
 );
--- store.ts
+++ store.ts
@@ -2,16 +2,27 @@ import { legacy_createStore as createStore } from "redux";
 import { combineReducers } from "redux";
 import { cartReducer } from "./src/redux/cart/reducers";
 import { productsReducer } from "./src/redux/products/reducers";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
 
 const rootReducer = combineReducers({
   cart: cartReducer,
   products: productsReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = createStore(
-  rootReducer,
+  persistedReducer,
   // @ts-ignore
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof rootReducer>;

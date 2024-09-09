6569b284f98f4e3cff4e03d924598d70563a907b
vishal-kumawat-appinventiv <vishal.kumawat@appinventiv.com>
2024-09-09 17:13:51 +0530

    commit

--- store.ts
+++ store.ts
@@ -8,6 +8,10 @@ const rootReducer = combineReducers({
   products: productsReducer,
 });
 
-export const store = createStore(rootReducer);
+export const store = createStore(
+  rootReducer,
+  // @ts-ignore
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export type RootState = ReturnType<typeof rootReducer>;

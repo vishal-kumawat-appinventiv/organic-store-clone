911c317da088420e360f1bbcdc86b7a04a097cbb
vishal-kumawat-appinventiv <vishal.kumawat@appinventiv.com>
2024-09-10 19:08:14 +0530

    commit

--- src/redux/rootSaga.ts
+++ src/redux/rootSaga.ts
@@ -1,6 +1,6 @@
-import { all } from "redux-saga/effects";
+import { all, fork } from "redux-saga/effects";
 import { watchLoadProducts } from "./products/sagas";
 
 export default function* rootSaga() {
-  yield all([watchLoadProducts()]);
+  yield all([fork(watchLoadProducts)]);
 }

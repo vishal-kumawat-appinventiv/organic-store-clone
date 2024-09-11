69914b69e9490c7edfbbcd7bd1dc23de3e584250
vishal-kumawat-appinventiv <vishal.kumawat@appinventiv.com>
2024-09-11 10:58:56 +0530

    react-loader added

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,10 @@
+import { PulseLoader } from "react-spinners";
+
 const Loading = () => {
   return (
-    <div className="my-14">
-      <h1 className="text-3xl font-bold text-center">Loading...</h1>
+    <div className="my-14 flex gap-4 items-center justify-center w-full">
+      <h1 className="text-3xl font-bold text-center">Loading</h1>
+      <PulseLoader />
     </div>
   );
 };
